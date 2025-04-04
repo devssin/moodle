7f03b24d4b45c05a1346b28adccc677a5047e37c
Yaasine AAYNE ALHAYATE <65537725+devssin@users.noreply.github.com>
2025-04-04 15:21:08 +0000

    Update config.php

--- config.php
+++ config.php
@@ -56,7 +56,7 @@ $CFG->dboptions = array(
                                 //  (please note mysql is always using socket
                                 //  if dbhost is 'localhost' - if you need
                                 //  local port connection use '127.0.0.1')
-    'dbport'    => '',          // the TCP port number to use when connecting
+    'dbport'    => getenv('DB_PORT'),          // the TCP port number to use when connecting
                                 //  to the server. keep empty string for the
                                 //  default port
     'dbhandlesoptions' => false,// On PostgreSQL poolers like pgbouncer don't
